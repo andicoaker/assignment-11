e5a5a7f11f62654974112cb26bee7a4a3b1d0e95
andicoaker <Andi@Andreas-MacBook.local>
2017-01-28 12:56:54 -0500

    solved ex-02, starting ex-03

--- ex-02-hasDoubleLetters.js
+++ ex-02-hasDoubleLetters.js
@@ -4,14 +4,26 @@
  *
  * Write a function called `hasDoubleLetters` that takes a string as an argument and returns true if the string has double letters next to eachother, or returns false
  *
- *
 **/
 
 // INPUT: string
-// OUTPUT: boolean - if string has double letters next to each other
+// OUTPUT: boolean (reduce) - if string has double letters next to each other
+// LOGIC:  convert string toLowerCase, then iterate over each index position to see if it equals the next index position
 
+var hasDoubleLetters = function (inputStr){
+  var dblLetters = false
 
+  for (var i = 0; i < inputStr.length; i++) {
+    inputStr = inputStr.toLowerCase()
+      // console.log(inputStr.toLowerCase());
 
+    if(inputStr[i] === inputStr[i + 1]){
+      dblLetters = true;
+      // console.log(inputStr[i]);
+    }
+  }
+  return dblLetters
+}
 
 console.assert( hasDoubleLetters('stutter') === true )
 console.assert( hasDoubleLetters('prospect') === false )
--- ex-03-maxOfArray.js
+++ ex-03-maxOfArray.js
@@ -4,6 +4,26 @@
 // Write a function maxOfArray() that takes an array of
 // numbers as an argument and finds the highest number.
 
+// INPUT: array of numbers
+// OUTPUT: highest number
+// LOGIC: if index position is greater than next index position, return that index
+
+var maxOfArray = function (arrOfNum){
+  var highestNum = 0
+
+  for (var i = 0; i < arrOfNum.length; i++) {
+    console.log(arrOfNum[i]);
+
+    if (arrOfNum[i] > arrOfNum[i + 1] ) {
+      console.log( arrOfNum[i] > arrOfNum[i + 1] );
+
+      highestNum = arrOfNum[i + 1];
+    }
+
+  }
+  return highestNum
+}
+
 
 console.assert(maxOfArray([2,7,3,4,21,0]) === 21)
 console.assert(maxOfArray([100,9,8,7,6,10]) === 100)
